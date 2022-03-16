c1a7505c14965eed16fa372f4ad35ad1bc42dd3e
Rvn0xsy <rvn0xsy@gmail.com>
2022-03-16 21:59:58 +0800

    增加C++ COM调用代码

---
+++ SystemGapCOMCall/SystemGapCOMCall.cpp
@@ -0,0 +1,30 @@
+﻿// SystemGapCOMCall.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <combaseapi.h>
+#include <Windows.h>
+#include <assert.h>
+#include <atlbase.h>
+
+int main()
+{
+    HRESULT hr = CoInitialize(NULL);
+
+    // ProgId值存放
+    CLSID clsid;
+
+    // 通过 ProgID 取得组件的 CLSID
+    hr = ::CLSIDFromProgID(L"SystemGapCOM.SystemGapObject", &clsid);
+    assert(hr != S_OK);
+
+    CComPtr<IUnknown>UnkPtr;
+
+    hr = ::CoCreateInstance(clsid, NULL, CLSCTX_ALL, IID_IUnknown, (LPVOID*)&UnkPtr);
+    CComDispatchDriver DispatchInterface(UnkPtr);
+
+    hr = DispatchInterface.InvokeN((LPCOLESTR)L"Create", NULL, NULL, NULL);
+
+    assert(hr != S_OK);
+
+    CoUninitialize();
+}
