7227a7fddc54b3f467be9791a842a3963e3089bc
Rvn0xsy <rvn0xsy@gmail.com>
2022-03-16 11:14:54 +0800

    增加COM组件版本，可以尝试远程调用

---
+++ SystemGapCOM/dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h: 模块类的声明。
+
+class CSystemGapCOMModule : public ATL::CAtlDllModuleT< CSystemGapCOMModule >
+{
+public :
+	DECLARE_LIBID(LIBID_SystemGapCOMLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_SYSTEMGAPCOM, "{9ea50fc7-07b8-4374-a8df-41eb34485c55}")
+};
+
+extern class CSystemGapCOMModule _AtlModule;
